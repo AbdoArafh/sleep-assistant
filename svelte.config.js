a2f0f9e8442e9c89bceb202c8b67d3141b602a48
Abdurrahman <abdoarafh54@gmail.com>
2022-08-24 06:55:46 +0200

    added: gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +10,18 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
+		paths: {
+			base: '/sleep-assistant'
+		},
+
+		prerender: {
+			default: true
 		}
 	}
 };
